d1646f0e2bd279bb71a268953bc73337f2923c00
mreshta <mreshta10@gmail.com>
2023-08-14 22:45:27 -0700

    This is the third test class

---
+++ src/TestProject/Test3.java
@@ -0,0 +1,11 @@
+package TestProject;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+	System.out.println("This is Test #3");
+	System.out.println("This is Test #3");
+	System.out.println("This is Test #3");
+	}
+
+}
